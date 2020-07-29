7c4431ff80170d327a3272b5f57aa22b489b7589
moisesu05 <moisesronanl@gmail.com>
2020-07-29 11:14:42 +0800

    sdasd

--- src/Components/GalleryPg/Gallery.js
+++ src/Components/GalleryPg/Gallery.js
@@ -2,22 +2,27 @@ import React from 'react'
 
 function Gallery() {
     return (
-        <div>
+        <div className="gal-all body">
             <h2>GALLERY</h2>
-            <section className='gall-sec'>
-                <div class="frame">
-                    <div class="img"></div>
-                    <div class="border"><h1>+</h1></div>
+            <section class="gallery">
+                <div class="container">
+                    <div class="grid">
+                        <div class="column-xs-12 column-md-4">
+                            <figure class="img-container">
+                            <img src="https://source.unsplash.com/8b1cWDyvT7Y" />
+                            <figcaption class="img-content">
+                                <h2 class="title">Smart Watch</h2>
+                                <h3 class="category">Showcase</h3>
+                            </figcaption>
+                            <span class="img-content-hover">
+                                <h2 class="title">Smart Watch</h2>
+                                <h3 class="category">Showcase</h3>
+                            </span>
+                            </figure>
+                        </div>
+                    </div>
                 </div>
-                <div class="frame">
-                    <div class="img"></div>
-                    <div class="border"><h1>+</h1></div>
-                </div>
-                <div class="frame">
-                    <div class="img"></div>
-                    <div class="border"><h1>+</h1></div>
-                </div>
-            </section>
+            </section>    
         </div>
     )
 }
